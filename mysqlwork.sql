f3988b4bfe65aa846031d534380150e816e06bb5
Darian-Frey <shane.hartley06@gmail.com>
2021-04-27 20:32:28 +0000

    How Python Talks To Data  Python and MySQL  Python and MySQL Introduction

--- mysqlwork.sql
+++ mysqlwork.sql
@@ -247,3 +247,15 @@ DESC LIMIT 5;
 SELECT SUM(InvoiceLine.UnitPrice * InvoiceLine.Quantity), Track.Name AS Track FROM InvoiceLine
 JOIN Track ON InvoiceLine.TrackId = Track.TrackId
 WHERE Track.Name = "The Woman King";
+
+
+Insert into Track (Name, AlbumId,MediaTypeId , GenreId, Composer, Milliseconds, Bytes, UnitPrice)
+            values("Extra Track", 348, 2, 1, "U2", 290000, 1234, 0.99);
+
+
+INSERT INTO Track
+    (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice)
+VALUES
+    ("Another Time, Another Place", 348, 2, 1, "U2", 210000, 1234, 0.99),
+    ("The Electric Co.", 348, 2, 1, "U2", 200000, 1234, 0.99),
+    ("Shadows and Tall Trees", 348, 2, 1, "U2", 150000, 1234, 0.99);
